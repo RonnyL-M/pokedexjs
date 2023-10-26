bbe7a329e62e0426940938976d285832aa1961d6
Ronny <ronny.louis-marie@epitech.eu>
2023-10-26 16:59:56 +0200

    add pagination

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const btns = [];
 //creation des main div
 const div = document.createElement("div");
 div.setAttribute("id", "main")
@@ -9,20 +10,81 @@ tag.appendChild(text1)
 div.appendChild(tag);
 document.body.appendChild(div)
 
-createBtn = () =>{}
+//liste des pkmn
+const listpkmn = document.createElement("div");
+div.setAttribute("id", "listpkmn");
+div.appendChild(listpkmn);
 
+
+//function btn
+createBtn = (name) => {
+    const button = document.createElement("button");
+    button.classList.add("pokebtn");
+    button.name = name;
+    button.value = name;
+    button.textContent = name;
+    btns.push(button);
+    return button;
+
+}
+
+//appel api 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
     .then(response => response.json())
     .then(data => {
-        const {results}=data ;
-        results.forEach(element => { 
+        const { results } = data;
+        results.forEach(element => {
             const pokemon = document.createElement("p");
             const pokename = document.createTextNode(element.name);
-            pokemon.appendChild(pokename);
-            div.appendChild(pokemon);
-            
-        });
+            //pokemon.appendChild(pokename);
+            listpkmn.appendChild(createBtn(element.name));
+
+        }
+
+        );
+        Array.from(btn).forEach((el) => {
+        })
+        
+        btns.forEach((el)=>{
+            console.log("ping")
+            el.addEventListener("click",()=>{
+                console.log(el.name);
+            })
+        })
     })
-    .catch(error => console.error('Erreur: ',error ))
+    .catch(error => console.error('Erreur: ', error));
+
+
+
+const btn = document.getElementsByClassName("pokebtn");
+
+
+
+
+//paginate
+let offset = 0;
+const divpaginate = document.createElement("div");
+div.setAttribute("id", "paginate");
+const btnprevious = document.createElement("button");
+btnprevious.name = "previous";
+btnprevious.textContent = "previous"
+btnprevious.setAttribute("id", "previous")
+const btnnext = document.createElement("button");
+btnnext.name = "next";
+btnnext.textContent = "next";
+btnnext.setAttribute("id", "next");
+divpaginate.appendChild(btnprevious);
+divpaginate.appendChild(btnnext);
+div.appendChild(divpaginate);
+
+btnnext.addEventListener("click",()=>{
+    offset+=20;
+    console.log(offset);
+})
+
+btnprevious.addEventListener("click",()=>{
+    offset-=20;
+    console.log(offset);
+})
 
 
