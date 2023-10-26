3c48a3ef3f70edeffd3c00a29563ba24f8de7dbb
Ronny <ronny.louis-marie@epitech.eu>
2023-10-26 11:20:25 +0200

    add base request

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //creation des main div
 const div = document.createElement("div");
-div.setAttribute("id","main")
+div.setAttribute("id", "main")
 
 //test ajout element
 const tag = document.createElement("p");
@@ -9,6 +9,20 @@ tag.appendChild(text1)
 div.appendChild(tag);
 document.body.appendChild(div)
 
+createBtn = () =>{}
 
+fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+    .then(response => response.json())
+    .then(data => {
+        const {results}=data ;
+        results.forEach(element => { 
+            const pokemon = document.createElement("p");
+            const pokename = document.createTextNode(element.name);
+            pokemon.appendChild(pokename);
+            div.appendChild(pokemon);
+            
+        });
+    })
+    .catch(error => console.error('Erreur: ',error ))
 
 
